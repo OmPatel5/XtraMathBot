bc977136380fce0b9558b78987d8d519ae9ddd5b
Om P <omp091216@gmail.com>
2022-04-27 18:05:47 -0400

    Saving to GitHub

--- xtraMathbot.py
+++ xtraMathbot.py
@@ -12,6 +12,7 @@ import time
 import pytesseract
 import cv2
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 #note that u have to run program 3 or 4 times after it is done and it shows ur progress and report on how many questions you got write and wrong exit out of it after and run code until it says u beat it
@@ -68,8 +69,6 @@ for i in range(50):
     inverted_img = cv2.bitwise_not(img)
     cv2.imwrite('C:\\Users\\omp16\\Documents\\new_folder\\inverted.png', inverted_img)
 
-    # cv2.imshow("inverted", inverted_img)
-    # cv2.waitKey(0)
 
     def grayscale(image):
         return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -77,17 +76,12 @@ for i in range(50):
     gray_image = grayscale(img)
     cv2.imwrite('C:\\Users\\omp16\\Documents\\new_folder\\gray.png', gray_image)
 
-    # cv2.imshow("gray", gray_image)
-    # cv2.waitKey(0)
 
     thresh, img_bw = cv2.threshold(gray_image, 180, 255, cv2.THRESH_BINARY)
     cv2.imwrite('C:\\Users\\omp16\\Documents\\new_folder\\bw_image.png', img_bw)
 
-    # cv2.imshow("black and white image", img_bw)
-    # cv2.waitKey(0)
 
     def noise_removal(image):
-        import numpy as np
         kernel = np.ones((1, 1), np.uint8)
         image = cv2.dilate(image, kernel, iterations=1)
         kernel = np.ones((1, 1), np.uint8)
@@ -99,11 +93,8 @@ for i in range(50):
     no_noise = noise_removal(img_bw)
     cv2.imwrite('C:\\Users\\omp16\\Documents\\new_folder\\no_noise.png', no_noise)
 
-    # cv2.imshow("no noise", no_noise)
-    # cv2.waitKey(0)
 
     def thin_font(image):
-        import numpy as np
         image = cv2.bitwise_not(image)
         kernel = np.ones((2, 2), np.uint8)
         image = cv2.erode(image, kernel, iterations=1)
@@ -117,9 +108,7 @@ for i in range(50):
     eroded_img = cv2.GaussianBlur(eroded_img,(11,11),0)
     eroded_img = cv2.medianBlur(eroded_img,9)
 
-    # cv2.imshow("eroded img", eroded_img)
-    # cv2.waitKey(0)
-
+  
     img = Image.fromarray(eroded_img)
     ocr1 = pytesseract.image_to_string(img, lang='eng', config='--psm 6-c tessedit_char_whitelist=0123456789')    
     ocr1_split = ocr1.split()
@@ -146,27 +135,18 @@ for i in range(50):
     inverted2_img = cv2.bitwise_not(img2)
     cv2.imwrite('C:\\Users\\omp16\\Documents\\new_folder\\inverted2.png', inverted2_img)
 
-    # cv2.imshow("inverted", inverted_img)
-    # cv2.waitKey(0)
-
 
     gray_image2 = grayscale(img2)
     cv2.imwrite('C:\\Users\\omp16\\Documents\\new_folder\\gray2.png', gray_image2)
 
-    # cv2.imshow("gray", gray_image)
-    # cv2.waitKey(0)
 
     thresh, img2_bw = cv2.threshold(gray_image2, 180, 255, cv2.THRESH_BINARY)
     cv2.imwrite('C:\\Users\\omp16\\Documents\\new_folder\\bw_image2.png', img2_bw)
 
-    # cv2.imshow("black and white image", img_bw)
-    # cv2.waitKey(0)
 
     no_noise2 = noise_removal(img2_bw)
     cv2.imwrite('C:\\Users\\omp16\\Documents\\new_folder\\no_noise2.png', no_noise2)
 
-    # cv2.imshow("no noise", no_noise)
-    # cv2.waitKey(0)
 
     eroded_img2 = thin_font(no_noise2)
     cv2.imwrite('C:\\Users\\omp16\\Documents\\new_folder\\eroded_img.png', eroded_img)
@@ -175,8 +155,7 @@ for i in range(50):
     eroded_img2 = cv2.GaussianBlur(eroded_img2,(11,11),0)
     eroded_img2 = cv2.medianBlur(eroded_img2,9)
 
-    # cv2.imshow("eroded img", eroded_img)
-    # cv2.waitKey(0)
+
 
     img2 = Image.fromarray(eroded_img2)
     ocr2 = pytesseract.image_to_string(img2, lang='eng', config='--psm 8-c tessedit_char_whitelist=0123456789')
